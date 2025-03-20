cd56dcf2f84b6404c1d19b3c36baa616d8380491
z50205 <z50205@yahoo.com.tw>
2025-03-21 02:00:35 +0800

    Part2 1st commit

--- taipei-day-trip/app.py
+++ taipei-day-trip/app.py
@@ -1,7 +1,9 @@
 from fastapi import *
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse,JSONResponse
 from routers import router
 app= FastAPI()
+app.mount("/static",StaticFiles(directory="static"),name="static")
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
