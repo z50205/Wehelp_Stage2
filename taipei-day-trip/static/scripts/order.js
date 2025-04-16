1f45fba4fc3100087a8788997929f1af4ba3e0c3
z50205 <z50205@yahoo.com.tw>
2025-04-17 00:33:38 +0800

    Change credit card form color when infos are invalid.

--- taipei-day-trip/static/scripts/order.js
+++ taipei-day-trip/static/scripts/order.js
@@ -14,6 +14,43 @@ TPDirect.card.setup({
             placeholder: 'CVV'
         }
     },
+    styles: {
+        // Style all elements
+        'input': {
+            'color': 'gray'
+        },
+        // Styling ccv field
+        'input.ccv': {
+            // 'font-size': '16px'
+        },
+        // Styling expiration-date field
+        'input.expiration-date': {
+            // 'font-size': '16px'
+        },
+        // Styling card-number field
+        'input.card-number': {
+            // 'font-size': '16px'
+        },
+        // style focus state
+        ':focus': {
+            // 'color': 'black'
+        },
+        // style valid state
+        '.valid': {
+            'color': 'green'
+        },
+        // style invalid state
+        '.invalid': {
+            'color': 'red'
+        },
+        // Media queries
+        // Note that these apply to the iframe, not the root window.
+        '@media screen and (max-width: 400px)': {
+            'input': {
+                'color': 'orange'
+            }
+        }
+    },
     // 此設定會顯示卡號輸入正確後，會顯示前六後四碼信用卡卡號
     isMaskCreditCardNumber: true,
     maskCreditCardNumberRange: {
